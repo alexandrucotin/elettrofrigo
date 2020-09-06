f8061bc149ddecaf3d85192efe0ccfbcbf829062
Alexandru Cotin <a.cotin96@gmail.com>
2020-09-06 17:49:41 +0200

    Fix:video

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var size = {
+  width: window.innerWidth || document.body.clientWidth,
+  height: window.innerHeight || document.body.clientHeight,
+};
+document.getElementById("myVideo").style.width = size.width;
+document.getElementById("myVideo").style.height = size.height;
