b17a0fdd5789ca7d4b1416ab34308bf1132ff2af
Alexandru Cotin <a.cotin96@gmail.com>
2020-09-19 17:39:38 +0200

    made mobile versione

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var size = {
-  width: window.innerWidth || document.body.clientWidth,
-  height: window.innerHeight || document.body.clientHeight,
-};
-document.getElementById("myVideo").style.width = size.width;
-document.getElementById("myVideo").style.height = size.height;
-
